f05b9aa455ac53677fcfe095318f92c228c67fb6
iamstar7277 <iamstar7277@gmail.com>
2019-03-25 15:06:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pc/kakao_login/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.pc.kakao_login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.callback.UnLinkResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private String TAG = "MainActivity";
+
+
+    private SessionCallback callback;
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        kakaoData();
+    }
+    /** 카카오톡 **/
+    private void kakaoData(){
+        //로그아웃
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickLogout();
+            }
+        });
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+
+/** 토큰 만료시 갱신을 시켜준다**/
+        if(Session.getCurrentSession().isOpenable()){
+            Session.getCurrentSession().checkAndImplicitOpen();
+        }
+
+        Log.e(TAG, "토큰큰 : " + Session.getCurrentSession().getTokenInfo().getAccessToken());
+        Log.e(TAG, "토큰큰 리프레쉬토큰 : " + Session.getCurrentSession().getTokenInfo().getRefreshToken());
+        Log.e(TAG, "토큰큰 파이어데이트 : " + Session.getCurrentSession().getTokenInfo().getRemainingExpireTime());
+    }
+
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            Log.e(TAG, "카카오 로그인 성공 ");
+            requestMe();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Log.e(TAG, "exception : " + exception);
+            }
+        }
+    }
+
+    /** 사용자에 대한 정보를 가져온다 **/
+    private void requestMe() {
+
+        List<String> keys = new ArrayList<>();
+        keys.add("properties.nickname");
+        keys.add("properties.profile_image");
+        keys.add("kakao_account.email");
+
+        UserManagement.getInstance().me(keys, new MeV2ResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                super.onFailure(errorResult);
+                Log.e(TAG, "requestMe onFailure message : " + errorResult.getErrorMessage());
+            }
+
+            @Override
+            public void onFailureForUiThread(ErrorResult errorResult) {
+                super.onFailureForUiThread(errorResult);
+                Log.e(TAG, "requestMe onFailureForUiThread message : " + errorResult.getErrorMessage());
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.e(TAG, "requestMe onSessionClosed message : " + errorResult.getErrorMessage());
+            }
+
+            @Override
+            public void onSuccess(MeV2Response result) {
+                Log.e(TAG, "requestMe onSuccess message : " + result.getKakaoAccount().getEmail() + " " + result.getId() + " " + result.getNickname());
+            }
+
+        });
+    }
+
+    /** 로그아웃시 **/
+    private void onClickLogout() {
+
+        UserManagement.getInstance().requestUnlink(new UnLinkResponseCallback() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.e(TAG, "카카오 로그아웃 onSessionClosed");
+            }
+
+            @Override
+            public void onNotSignedUp() {
+                Log.e(TAG, "카카오 로그아웃 onNotSignedUp");
+            }
+
+            @Override
+            public void onSuccess(Long result) {
+                Log.e(TAG, "카카오 로그아웃 onSuccess");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
+
+
+
